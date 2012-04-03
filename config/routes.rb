46d5205810e40a0792fd914e86726e173ba4e218
Elaine Chang <elainefchang@gmail.com>
2012-04-03 00:16:14 -0700

    Devise works

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Socialteeth3::Application.routes.draw do
+  get "home/index"
+
   resources :ads
+  
+  
+  root :to => 'home#index'
 
   devise_for :users
 
@@ -52,7 +57,7 @@ Socialteeth3::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+
 
   # See how all your routes lay out with "rake routes"
 
