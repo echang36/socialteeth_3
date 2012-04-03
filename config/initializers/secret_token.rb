d61c56288eedd424587e949b4cc9585d326ab333
Elaine Chang <elainefchang@gmail.com>
2012-04-03 00:03:18 -0700

    Installed Devise, generated scaffold for Ads

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialteeth3::Application.config.secret_token = 'f237156719ef2e0422996edc6fa1eb98cfa5a171579fb0d7c26de007512d1e73d414e00edeb91864bc646c7eda43a16d2aa7fa6aabb0ad58e0ce3d7a96f09da8'
