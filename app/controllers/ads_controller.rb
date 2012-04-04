fc0a6accb8823ff02e19a72e71f21598cb6b0ad8
Elaine Chang <elainefchang@gmail.com>
2012-04-04 14:56:02 -0700

    deleted @ad instance

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -6,7 +6,7 @@ class AdsController < ApplicationController
   # GET /ads
   # GET /ads.json
   def index
-    @ads = Ad.all
+
 
     respond_to do |format|
       format.html # index.html.erb
@@ -19,7 +19,7 @@ class AdsController < ApplicationController
   # GET /ads/1
   # GET /ads/1.json
   def show
-    @ad = Ad.find(params[:id])
+
 	@ad_contributions = Contribution.where(ad_id: @ad.id)
 	@raised=0
 	@ad_contributions.each {|a| @raised += a.amount}
@@ -40,7 +40,6 @@ class AdsController < ApplicationController
   # GET /ads/new
   # GET /ads/new.json
   def new
-    @ad = Ad.new
 
     respond_to do |format|
       format.html # new.html.erb
@@ -72,15 +71,8 @@ class AdsController < ApplicationController
   
   # GET /ads/1/edit
   def edit
-    @ad = Ad.find(params[:id])
 	
 	
-	if !check_if_owner(@ad.user_id)
-		respond_to do |format|
-			format.html { redirect_to @ad, notice: 'Incorrect User. Must be owner of Ad to edit.'} 
-			format.json { render json: @ad.errors, status: :unprocessable_entity }
-		end
-	end
   end
 
 
@@ -88,15 +80,6 @@ class AdsController < ApplicationController
   # PUT /ads/1.json
   def update
 
-    @ad = Ad.find(params[:id])
-	if !check_if_owner(@ad.user_id)
-		respond_to do |format|
-			format.html { redirect_to @ad, notice: 'Incorrect User. Must be owner of Ad to edit.'} 
-			format.json { render json: @ad.errors, status: :unprocessable_entity }
-		end
-		return
-	end
-	
 	respond_to do |format|
 	    if @ad.update_attributes(params[:ad])
 			format.html { redirect_to @ad, notice: 'Ad was successfully updated.' }
@@ -115,14 +98,7 @@ class AdsController < ApplicationController
   # DELETE /ads/1
   # DELETE /ads/1.json
   def destroy
-    @ad = Ad.find(params[:id])
-	if !check_if_owner(@ad.user_id)
-		respond_to do |format|
-			format.html { redirect_to @ad, notice: 'Incorrect User. Must be owner of Ad to edit.'} 
-			format.json { render json: @ad.errors, status: :unprocessable_entity }
-		end
-		return
-	end
+	
     @ad.destroy
 
     respond_to do |format|
@@ -132,11 +108,5 @@ class AdsController < ApplicationController
   end
   
   
-  protected
-	def check_if_owner (auser_id)
-		if current_user.id != auser_id
-			return false
-		end
-	end
-	
+
 end
